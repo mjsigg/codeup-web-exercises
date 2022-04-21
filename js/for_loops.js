953e867b6730f6e0d02a27089db47b2c975bbd45
Michael J Sigg <mjsigg@gmail.com>
2022-04-21 15:20:25 -0500

    finally solved first problem

---
+++ js/for_loops.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function showMultiplicationTable (input) {
+    for (let x = 0; x < 10; x++){
+        console.log(input + ' * ' + x + ' = ' + input * x)
+    }
+}
+
+showMultiplicationTable(7)
+
